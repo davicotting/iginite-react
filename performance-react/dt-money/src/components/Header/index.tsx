3af8e97d8f7fa7c3b5fd4692d43fcfa43e0cdfcb
Davi Thomeny Cotting <122418493+davicotting@users.noreply.github.com>
2024-11-18 19:01:44 -0300

    .

---
+++ performance-react/dt-money/src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import logo from "../../assets/dt-money-logo.svg";
+
+export function Header(){
+    return(
+        <HeaderContainer>
+            <HeaderContent>
+                <img src={logo} alt="" />
+
+                <NewTransactionButton>Nova transação</NewTransactionButton>
+            </HeaderContent>
+        </HeaderContainer>
+    )
+}
